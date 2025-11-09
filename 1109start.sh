d8125be791bfebee8f14bb0274e7254acbde81ec
JinzeLi <jinzeli2@amd.com>
2025-11-09 13:26:27 +0000

    1109

---
+++ 1109start.sh
@@ -0,0 +1,5 @@
+export HF_HOME='/wekafs/jinzeli2/cache'
+export HF_HUB_OFFLINE='1'
+export HIP_VISIBLE_DEVICES='0'
+
+llamafactory-cli train examples/train_lora/llama3_1_8b_lora_sft_accept_head.yaml
